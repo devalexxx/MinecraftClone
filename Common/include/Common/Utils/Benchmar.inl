8b2344fefa8ca8498b693aa0aa05c589dc63f787
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-07-15 01:44:51 +0200

    feat(Benchmark): add utility tool to benchmark function

---
+++ Common/include/Common/Utils/Benchmar.inl
@@ -0,0 +1,25 @@
+//
+// Created by Alex Clorennec on 14/07/2025.
+//
+
+#include "Common/Utils/Logging.h"
+
+namespace Mcc::_
+{
+
+    template<typename Tag>
+    TimeBenchmark<Tag>::~TimeBenchmark()
+    {
+        MCC_LOG_DEBUG("[TimeBench - {}] called {} times, total: {:.2f}, avg: {:.5f}", Tag::name(), callCount, timeSpent, timeSpent / callCount);
+    }
+
+    template<typename Tag>
+    void TimeBenchmark<Tag>::AddCall(const float elapsed)
+    {
+        std::lock_guard l(mutex);
+        timeSpent += elapsed;
+        ++callCount;
+    }
+
+
+}
---
+++ Common/include/Common/Utils/Benchmark.h
@@ -0,0 +1,58 @@
+//
+// Created by Alex Clorennec on 14/07/2025.
+//
+
+#ifndef MCC_COMMON_UTILS_BENCHMARK_H
+#define MCC_COMMON_UTILS_BENCHMARK_H
+
+#include <mutex>
+
+#if defined(MCC_DEBUG) || defined(MCC_BENCH)
+    static_assert(MCC_LOG_LEVEL >= MCC_DEBUG_LEVEL);
+#   define MCC_BENCH_TIME(NAME, CALLABLE)                                                                    \
+        [&]() {                                                                                              \
+            struct __Tag##__LINE__ { static constexpr const char* name() { return #NAME; } };                \
+            static Mcc::_::TimeBenchmark<__Tag##__LINE__> __benchmark##__LINE__;                             \
+            return [=](auto&&... args) -> decltype(auto) {                                                   \
+                const auto __start = TimeClock::now();                                                       \
+                if constexpr (std::is_void_v<std::invoke_result_t<decltype(CALLABLE), decltype(args)...>>)   \
+                {                                                                                            \
+                    CALLABLE(std::forward<decltype(args)>(args)...);                                         \
+                    const auto __end = TimeClock::now();                                                     \
+                    float __duration = std::chrono::duration<float>(__end - __start).count();                \
+                    __benchmark##__LINE__.AddCall(__duration);                                               \
+                }                                                                                            \
+                else                                                                                         \
+                {                                                                                            \
+                    auto __result = CALLABLE(std::forward<decltype(args)>(args)...);                         \
+                    const auto __end = TimeClock::now();                                                     \
+                    float __duration = std::chrono::duration<float>(__end - __start).count();                \
+                    __benchmark##__LINE__.AddCall(__duration);                                               \
+                    return __result;                                                                         \
+                }                                                                                            \
+            };                                                                                               \
+        }()
+#else
+#   define MCC_BENCH_TIME(NAME, CALLABLE) CALLABLE
+#endif
+
+namespace Mcc::_
+{
+
+    template<typename Tag>
+    struct TimeBenchmark
+    {
+        ~TimeBenchmark();
+
+        void AddCall(float elapsed);
+
+        std::mutex mutex    {};
+        float      timeSpent{};
+        size_t     callCount{};
+    };
+
+}
+
+#include "Common/Utils/Benchmar.inl"
+
+#endif
