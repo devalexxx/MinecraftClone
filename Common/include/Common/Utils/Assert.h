33600e1fb13ed59e4f003d13ca09bff01712811c
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-10-22 15:13:09 +0200

    chore: add assert log in NDEBUG mode with MCC_RELEASE_DEBUG

--- Common/include/Common/Utils/Assert.h
+++ Common/include/Common/Utils/Assert.h
@@ -14,16 +14,18 @@
 #   define PRETTY_FUNCTION __PRETTY_FUNCTION__
 #endif
 
-#define MCC_ASSERT_IMPL(expr, format, ...)                                                      \
+#define MCC_ASSERT_IMPL(fatal, expr, format, ...)                                                      \
     (!(expr)) ?                                                                                 \
         (fmt::print(stderr, fg(fmt::color::red), "Assertion failed at {}:{} : {}\n",            \
         __FILE__, __LINE__, PRETTY_FUNCTION),                                                   \
         fmt::print(stderr, fg(fmt::color::yellow), "\tassert  : \"{}\"\n\tmessage : ", #expr),  \
         fmt::print(stderr, fg(fmt::color::yellow), format, ##__VA_ARGS__),                      \
-        fmt::print(stderr, "\n"), abort(), 0) : 1
+        fmt::print(stderr, "\n"), (fatal ? abort() : (void)0), 0) : 1
 
 #ifdef MCC_DEBUG
-#   define MCC_ASSERT(expr, fmt, ...) MCC_ASSERT_IMPL(expr, fmt, ##__VA_ARGS__)
+#   define MCC_ASSERT(expr, fmt, ...) MCC_ASSERT_IMPL(true, expr, fmt, ##__VA_ARGS__)
+#elif MCC_RELEASE_DEBUG
+#   define MCC_ASSERT(expr, fmt, ...) MCC_ASSERT_IMPL(false, expr, fmt, ##__VA_ARGS__)
 #else
 #   define MCC_ASSERT(expr, fmt, ...)
 #endif
